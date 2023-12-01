7ecaf6903fdb9d825fd031b1527a00297269756c
beoun_vichet <beounvichet@gmail.com>
2023-12-01 11:35:38 +0700

    updating gradle wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,20 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('ssss')),
-          backgroundColor: Colors.lightBlue,
-        ),
         backgroundColor: Colors.redAccent,
-        bottomNavigationBar: Text('TEXT'),
         floatingActionButton: FloatingActionButton(
           backgroundColor: Colors.lightGreenAccent, child: Icon(Icons.add),
           onPressed: () {print(Text('test'));},
+        ),
+        body: SafeArea(  //SafeArea ប្រើកុំអោយ Text ទៅចុងខាងលើពេក
+          child: Container( //Container ដាក់បាន child: តែ ១ ទេ
+            height: 100.0, //100.0 = px
+            width: 100.0,
+            margin: EdgeInsets.fromLTRB(20.0,20.0,10,0),  //symmetric ver&hor only , fromLTRB(right,l,t,b), only=តែ១ផ្នែក រុញពីក្រៅ
+            padding: EdgeInsets.only(left: 20.0), //រុញពីក្នុង
+            child: Text('HELLO'),
+            color: Colors.white,
+          ),
         ),
       ),
     );
